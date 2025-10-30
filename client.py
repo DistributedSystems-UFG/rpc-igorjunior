382e5ec828827f765d87d7ae0b42ad65d3a4da62
Igor Junior <igorjunior@pm.me>
2025-10-30 15:28:36 -0300

    Release

--- client.py
+++ client.py
@@ -3,7 +3,39 @@ from constRPYC import * #-
 
 class Client:
   conn = rpyc.connect(SERVER, PORT) # Connect to the server
-  print (conn.root.exposed_value())
-  conn.root.exposed_append(5)       # Call an exposed operation,
-  conn.root.exposed_append(6)       # and append two elements
-  print (conn.root.exposed_value())   # Print the result
+  print("Lista inicial:", conn.root.exposed_value())
+  
+  # Append
+  conn.root.exposed_append(5)
+  conn.root.exposed_append(6)
+  conn.root.exposed_append(3)
+  conn.root.exposed_append(8)
+  print("Após append(5, 6, 3, 8):", conn.root.exposed_value())
+  
+  # Pesquisa
+  print("Busca por 6:", conn.root.exposed_search(6))
+  print("Busca por 10:", conn.root.exposed_search(10))
+  
+  # Insert
+  conn.root.exposed_insert(2, 99)
+  print("Após insert(2, 99):", conn.root.exposed_value())
+  
+  # Sort
+  conn.root.exposed_sort()
+  print("Após sort():", conn.root.exposed_value())
+  
+  # Reverse
+  conn.root.exposed_reverse()
+  print("Após reverse():", conn.root.exposed_value())
+  
+  # Remove
+  success, lista = conn.root.exposed_remove(99)
+  print("Após remove(99):", lista, "- Sucesso:", success)
+  
+  # Pop
+  success, valor = conn.root.exposed_pop()
+  print("Após pop():", valor, "- Lista:", conn.root.exposed_value())
+  
+  # Clear
+  conn.root.exposed_clear()
+  print("Após clear():", conn.root.exposed_value())
--- server.py
+++ server.py
@@ -11,6 +11,41 @@ class DBList(rpyc.Service):
 
   def exposed_value(self):
     return self.value
+  
+  def exposed_search(self, data):
+    try:
+      return self.value.index(data)
+    except ValueError:
+      return -1
+  
+  def exposed_remove(self, data):
+    try:
+      self.value.remove(data)
+      return True, self.value
+    except ValueError:
+      return False, self.value
+  
+  def exposed_insert(self, index, data):
+    self.value.insert(index, data)
+    return self.value
+  
+  def exposed_sort(self, reverse=False):
+    self.value.sort(reverse=reverse)
+    return self.value
+  
+  def exposed_clear(self):
+    self.value = []
+    return self.value
+  
+  def exposed_reverse(self):
+    self.value.reverse()
+    return self.value
+  
+  def exposed_pop(self, index=-1):
+    try:
+      return True, self.value.pop(index)
+    except IndexError:
+      return False, None
 
 if __name__ == "__main__":
   server = ThreadedServer(DBList(), port = PORT)
